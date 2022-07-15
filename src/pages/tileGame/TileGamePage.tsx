7867340cfe41d083b8e5d8d4c032d67991d7fb14
Lincoln Anderson <landerson@threefivetwo.com>
2022-07-14 23:07:16 -0400

    add tile colors

--- src/pages/tileGame/TileGamePage.tsx
+++ src/pages/tileGame/TileGamePage.tsx
@@ -5,62 +5,140 @@ import { useDrag, useDrop } from 'react-dnd';
 import useDoubleClick from 'use-double-click';
 
 import { NonScrollingFlex } from './NonScrollingFlex';
-import { ItemTypes, Tile, GameState, boardSize } from './game';
+import { ItemTypes, Tile, GameState, boardSize, traySize } from './game';
 import CustomDragLayer from '../dnd/dnd-helpers/CustomDragLayer';
 import DndMultiProvider from '../dnd/dnd-helpers/DndMultiProvider';
 import { useDisableNativePreview } from '../dnd/dnd-helpers/hooks';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
 
-const TileView: React.FC<{ tile: Tile } & UI.BoxProps> = ({
-  tile,
-  ...boxProps
-}) => {
-  // Inform dnd that we can drag this tile
-  const [{ isDragging }, drag, preview] = useDrag(
-    {
-      type: ItemTypes.tile,
-      item: tile,
-      collect: (monitor) => ({
-        isDragging: monitor.isDragging(),
-      }),
-      // options: {
-      //   dropEffect: 'grabbing',
-      // },
-    },
-    [tile]
-  );
-  useDisableNativePreview(preview);
-  const buttonRef = React.useRef<HTMLDivElement | null>(null);
-  useDoubleClick({
-    onDoubleClick: (e) => {
-      window.alert('double click');
-    },
-    ref: buttonRef,
-  });
+const TileView: React.FC<{ tile: Tile; isDragLayer?: boolean } & UI.BoxProps> =
+  ({ tile, isDragLayer, ...boxProps }) => {
+    // Inform dnd that we can drag this tile
+    const [{ isDragging }, drag, preview] = useDrag(
+      {
+        type: ItemTypes.tile,
+        item: tile,
+        collect: (monitor) => ({
+          isDragging: monitor.isDragging(),
+        }),
+      },
+      [tile]
+    );
+    useDisableNativePreview(preview);
+    const buttonRef = React.useRef<HTMLDivElement | null>(null);
+    useDoubleClick({
+      onSingleClick: (e) => {
+        console.log('single click');
+      },
+      onDoubleClick: (e) => {
+        console.log('double click');
+      },
+      ref: buttonRef,
+    });
 
-  return (
-    <UI.Box
-      ref={drag}
-      as="button"
-      w="80px"
-      h="80px"
-      bg="gray.500"
-      color="gray.200"
-      fontWeight="bold"
-      fontSize="lg"
-      borderRadius="md"
-      opacity={isDragging ? 0 : 1}
-      cursor="grab"
-      sx={{
-        WebkitTapHighlightColor: 'rgba(0,0,0,0)',
-      }}
-      {...boxProps}
-    >
-      <UI.Box ref={buttonRef} lineHeight="80px">
-        {tile.id}
+    return (
+      <UI.Box
+        ref={drag}
+        as="button"
+        w="80px"
+        h="80px"
+        borderRadius="lg"
+        position="relative"
+        opacity={isDragging ? 0 : 1}
+        cursor="grab"
+        sx={{
+          WebkitTapHighlightColor: 'rgba(0,0,0,0)',
+        }}
+        boxShadow={isDragLayer ? 'dark-lg' : ''}
+        top={isDragLayer ? '-20px' : 0}
+        {...boxProps}
+      >
+        <UI.Flex
+          alignItems="end"
+          justifyContent="space-between"
+          w="80px"
+          h="100px"
+          borderRadius="lg"
+          bg="gray.600"
+          position="absolute"
+          top={0}
+          left={0}
+          visibility={isDragLayer ? 'visible' : 'hidden'}
+          px={1}
+          fontSize="8px"
+          color="gray.600"
+        >
+          <UI.HStack spacing="2px" px={1} py="5px">
+            {_.map(tile.mods, (mod, key) => {
+              if (mod.value <= 0) return null;
+
+              return (
+                <React.Fragment key={key}>
+                  {_.times(mod.value, (i) => {
+                    return (
+                      <UI.Box
+                        key={i}
+                        bg={mod.color.hex}
+                        borderRadius="2px"
+                        w={3}
+                        h={3}
+                      >
+                        <FontAwesomeIcon icon={faPlus} />
+                      </UI.Box>
+                    );
+                  })}
+                </React.Fragment>
+              );
+            })}
+          </UI.HStack>
+          <UI.HStack spacing="2px" px={1} py="5px">
+            {_.map(tile.mods, (mod, key) => {
+              if (mod.value >= 0) return null;
+
+              return (
+                <React.Fragment key={key}>
+                  {_.times(-mod.value, (i) => {
+                    return (
+                      <UI.Box
+                        key={i}
+                        bg={mod.color.hex}
+                        borderRadius="2px"
+                        w={3}
+                        h={3}
+                      >
+                        <FontAwesomeIcon icon={faMinus} />
+                      </UI.Box>
+                    );
+                  })}
+                </React.Fragment>
+              );
+            })}
+          </UI.HStack>
+        </UI.Flex>
+        <UI.Box
+          w="80px"
+          h="80px"
+          borderRadius="lg"
+          bg={tile.color.hex}
+          color="gray.200"
+          position="absolute"
+          top={0}
+          left={0}
+          transform={isDragLayer ? 'scale(0.95)' : ''}
+        >
+          <UI.Box
+            ref={buttonRef}
+            fontWeight="bold"
+            fontSize="lg"
+            lineHeight="80px"
+          >
+            {/* {tile.id} */}
+          </UI.Box>
+        </UI.Box>
       </UI.Box>
-    </UI.Box>
-  );
-};
+    );
+  };
 
 const TileSlot: React.FC<
   { index: number; mode?: 'board' | 'tray' } & UI.GridProps
@@ -82,6 +160,13 @@ const TileSlot: React.FC<
 
   const tile = game.tiles[index];
 
+  const dropZoneColor =
+    dropState.isOver && dropState.canDrop
+      ? mode === 'tray'
+        ? 'gray.300'
+        : 'green.400'
+      : 'transparent';
+
   return (
     <UI.Grid
       ref={drop}
@@ -95,7 +180,7 @@ const TileSlot: React.FC<
       }}
       templateRows="1fr"
       templateColumns="1fr"
-      templateAreas={`"main"`}
+      templateAreas="main"
       alignItems="center"
       justifyItems="center"
       {...gridProps}
@@ -103,29 +188,46 @@ const TileSlot: React.FC<
       <UI.Box
         gridArea="main"
         borderRadius="full"
-        bg="gray.700"
+        bg={mode === 'tray' ? 'gray.400' : 'gray.100'}
         w={6}
         h={6}
         m={1}
-        opacity={0.25}
+        opacity={1}
+        boxShadow="inset 0 10px 20px rgba(0, 0, 0, 0.15)"
+      />
+      <UI.Box
+        gridArea="main"
+        borderRadius="lg"
+        border="2px dashed"
+        dash
+        borderColor={dropZoneColor}
+        w="76px"
+        h="76px"
+        m={1}
+        transition="all 0.15s"
       />
       <UI.Box
         gridArea="main"
         borderRadius="md"
-        border="2px dashed"
-        borderColor={
-          dropState.isOver && dropState.canDrop
-            ? mode === 'tray'
-              ? 'white'
-              : 'green.400'
-            : 'transparent'
-        }
-        w="76px"
-        h="76px"
+        bg={dropZoneColor}
+        w="68px"
+        h="68px"
         m={1}
+        opacity={0.3}
+        transition="all 0.15s"
       />
       {tile ? <TileView gridArea="main" zIndex={1} tile={tile} /> : null}
     </UI.Grid>
+  );
+};
+
+const Score: React.FC = () => {
+  const game = GameState.useContext();
+
+  return (
+    <UI.Box>
+      <UI.Text>{game.score}</UI.Text>
+    </UI.Box>
   );
 };
 
@@ -133,8 +235,15 @@ export const TileGamePage: React.FC = () => {
   return (
     <DndMultiProvider>
       <GameState.Provider value={null}>
-        {/* <LayoutGroup> */}
         <NonScrollingFlex.Root>
+          <NonScrollingFlex.Child
+            flex="1 0 auto"
+            alignItems="center"
+            justifyContent="center"
+            userSelect="none"
+          >
+            <Score />
+          </NonScrollingFlex.Child>
           <NonScrollingFlex.Child
             flex="1 0 auto"
             alignItems="center"
@@ -143,7 +252,7 @@ export const TileGamePage: React.FC = () => {
           >
             <UI.Box w="360px" h="360px" p="20px" pt="30px">
               <UI.SimpleGrid columns={4} spacing={0}>
-                {_.times(16, (i) => (
+                {_.times(boardSize, (i) => (
                   <TileSlot key={i} index={i} />
                 ))}
               </UI.SimpleGrid>
@@ -173,7 +282,7 @@ export const TileGamePage: React.FC = () => {
                 boxShadow="inset 0 10px 20px rgba(0, 0, 0, 0.15)"
               >
                 <UI.SimpleGrid columns={4} spacing={0}>
-                  {_.times(16, (i) => (
+                  {_.times(traySize, (i) => (
                     <TileSlot
                       key={i}
                       mode="tray"
@@ -186,11 +295,10 @@ export const TileGamePage: React.FC = () => {
             </UI.Flex>
           </NonScrollingFlex.Child>
         </NonScrollingFlex.Root>
-        {/* </LayoutGroup> */}
         <CustomDragLayer>
           {(dragLayer) => {
             return dragLayer.itemType === ItemTypes.tile ? (
-              <TileView tile={dragLayer.item} boxShadow="dark-lg" />
+              <TileView isDragLayer tile={dragLayer.item} />
             ) : null;
           }}
         </CustomDragLayer>
--- src/pages/tileGame/game.ts
+++ src/pages/tileGame/game.ts
@@ -11,23 +11,124 @@ export enum ItemTypes {
   tile = 'tile',
 }
 
+export interface TileColor {
+  hex: string;
+}
+
+export const tileColors: { [key: string]: TileColor } = {
+  grass: { hex: 'green.300' },
+  snow: { hex: 'gray.100' },
+  sand: { hex: 'orange.300' },
+  asphalt: { hex: 'gray.700' },
+  concrete: { hex: 'gray.500' },
+  forest: { hex: 'green.500' },
+};
+
+export type TileColorKey = keyof typeof tileColors;
+
 export interface Tile {
   id: number;
+  color: TileColor;
+  mods?: TileColorMods;
 }
 
 export type TilePosition = Tile | null;
+
+export interface TileColorMod {
+  color: TileColor;
+  value: number;
+}
+
+export interface TileColorMods {
+  [key: TileColorKey]: TileColorMod;
+}
+
+const getRandomColor = (): TileColor => {
+  return _.sample(Object.values(tileColors)) as TileColor;
+};
+
+const getRandomColorMods = (): TileColorMods => {
+  const mods = _.mapValues(tileColors, (color) => {
+    return {
+      color,
+      value: 0,
+    };
+  });
+
+  for (let i = 0; i < 4; i++) {
+    let color = _.sample(mods) as TileColorMod;
+    color.value += _.sample([-1, 0, 0, 1, 1, 1]) as number;
+  }
+
+  return mods;
+};
 
 const getInitialTiles = (): TilePosition[] => {
   return [
     ..._.times(boardSize, () => null),
     ..._.times(traySize, (i) => {
-      return { id: i };
+      return { id: i, color: getRandomColor(), mods: getRandomColorMods() };
     }),
   ];
 };
 
+const getTileToTileScore = (
+  tile: TilePosition,
+  tiles: TilePosition[],
+  index: number
+): number => {
+  if (tile === null || index < 0 || index >= boardSize) {
+    return 0;
+  }
+  const otherTile = tiles[index];
+  if (!otherTile) {
+    return 0;
+  }
+  const score = _.reduce(
+    tile.mods,
+    (acc, mod) => {
+      if (mod.color === otherTile.color) {
+        return acc + mod.value;
+      }
+      return acc;
+    },
+    0
+  );
+
+  return score;
+};
+
+const getTileScore = (tiles: TilePosition[], index: number): number => {
+  const tile = tiles[index];
+  if (!tile) {
+    return 0;
+  }
+
+  let score = 1;
+  score += getTileToTileScore(tile, tiles, index - 5);
+  score += getTileToTileScore(tile, tiles, index - 4);
+  score += getTileToTileScore(tile, tiles, index - 3);
+  score += getTileToTileScore(tile, tiles, index - 1);
+  score += getTileToTileScore(tile, tiles, index + 1);
+  score += getTileToTileScore(tile, tiles, index + 3);
+  score += getTileToTileScore(tile, tiles, index + 4);
+  score += getTileToTileScore(tile, tiles, index + 5);
+
+  return score;
+};
+
+const getScore = (tiles: TilePosition[]): number => {
+  let score = 0;
+  for (let i = 0; i < boardSize; i++) {
+    score += getTileScore(tiles, i);
+  }
+  return score;
+};
+
 export const GameState = createContextProvider(() => {
   const [tiles, setTiles] = React.useState<TilePosition[]>(getInitialTiles());
+
+  const score = getScore(tiles);
 
   const canMoveTile = (tile: Tile, toIndex: number) => {
     // TODO: immovable tiles
@@ -44,6 +145,7 @@ export const GameState = createContextProvider(() => {
 
   return {
     tiles,
+    score,
     canMoveTile,
     moveTile,
   };
