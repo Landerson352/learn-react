2f3a1733ca60984b96f67c65697f3f024689075c
Lincoln Anderson <landerson@threefivetwo.com>
2022-12-01 18:27:53 -0500

    update tile game ui

--- src/pages/tileGame/TileGamePage.tsx
+++ src/pages/tileGame/TileGamePage.tsx
@@ -6,6 +6,13 @@ import useDoubleClick from 'use-double-click';
 // import { useLongPress } from 'use-long-press';
 // import { useClickAway } from 'react-use';
 import * as FramerMotion from 'framer-motion';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCircle,
+  faMale,
+  faHeart,
+  faArrowsAlt,
+} from '@fortawesome/free-solid-svg-icons';
 
 import { NonScrollingFlex } from './NonScrollingFlex';
 import { ItemTypes, Tile, GameState, boardSize, traySize } from './game';
@@ -63,8 +70,8 @@ const TileView: React.FC<
         WebkitTapHighlightColor: 'rgba(0,0,0,0)',
       }}
       boxShadow={isDragLayer ? 'dark-lg' : ''}
-      top={isDragLayer ? '-20px' : 0}
-      transform={isDragLayer ? 'scale(0.95)' : ''}
+      // top={isDragLayer ? '-20px' : 0}
+      transform={isDragLayer ? 'scale(1.05)' : ''}
       {...boxProps}
     >
       {/* <UI.Flex
@@ -130,6 +137,7 @@ const TileView: React.FC<
           </UI.HStack>
         </UI.Flex> */}
       <UI.Box
+        key="edge"
         w="80px"
         h="80px"
         borderRadius="lg"
@@ -151,6 +159,7 @@ const TileView: React.FC<
       </UI.Box>
 
       <UI.Box
+        key="surface"
         w="80px"
         h="80px"
         borderRadius="lg"
@@ -162,22 +171,59 @@ const TileView: React.FC<
         left={0}
       />
 
-      <UI.Box
-        w="100px"
+      <UI.Flex
+        key="tooltip"
+        w="108px"
         h="40px"
         borderRadius="lg"
         // bg="pink"
         position="absolute"
         top="-50px"
-        left="-10px"
+        left="-14px"
         pointerEvents="none"
         bg="white"
         opacity={isDragLayer || mode === 'tray' ? 1 : 0}
         // opacity={isDragLayer ? 0 : Math.min(1, rendersCount)}
         // animate={{ opacity: isDragLayer ? 1 : 0 }}
         // transition={{ duration: 0.6 }}
-      />
+        alignItems="center"
+        justifyContent="center"
+      >
+        <UI.HStack>
+          <UI.HStack spacing="2px" color="orange.200" fontWeight="bold">
+            <FontAwesomeIcon icon={faCircle} />
+            <UI.Text color="orange.400">5</UI.Text>
+          </UI.HStack>
+          {/* <UI.HStack spacing="2px" color="green.300" fontWeight="bold">
+            <FontAwesomeIcon icon={faMale} />
+            <UI.Text color="green.500">1</UI.Text>
+          </UI.HStack> */}
+          <UI.HStack spacing="2px" color="red.300" fontWeight="bold">
+            <FontAwesomeIcon icon={faHeart} />
+            <FontAwesomeIcon icon={faArrowsAlt} />
+            <UI.Text color="red.500">2</UI.Text>
+          </UI.HStack>
+        </UI.HStack>
+      </UI.Flex>
     </UI.Box>
+  );
+};
+
+const slotSize = 80;
+const dropZoneSize = 84;
+const dropZoneBorderSize = dropZoneSize + 8;
+
+const TileSlotGrid: React.FC<
+  { columns?: number } & Omit<UI.SimpleGridProps, 'columns'>
+> = ({ columns = 4, ...restProps }) => {
+  return (
+    <UI.SimpleGrid
+      columns={columns}
+      spacing={0}
+      w={`${slotSize * columns}px`}
+      h={`${slotSize * columns}px`}
+      {...restProps}
+    />
   );
 };
 
@@ -204,15 +250,15 @@ const TileSlot: React.FC<
   const dropZoneColor =
     dropState.isOver && dropState.canDrop
       ? mode === 'tray'
-        ? 'gray.300'
-        : 'green.400'
+        ? 'yellow.400' // 'gray.600'
+        : 'yellow.400' // 'green.400'
       : 'transparent';
 
   return (
     <UI.Grid
       ref={drop}
-      w="80px"
-      h="80px"
+      w={`${slotSize}px`}
+      h={`${slotSize}px`}
       sx={{
         '>*': {
           flexShrink: 0,
@@ -221,43 +267,52 @@ const TileSlot: React.FC<
       }}
       templateRows="1fr"
       templateColumns="1fr"
-      templateAreas="main"
+      templateAreas={'"main"'}
       alignItems="center"
       justifyItems="center"
       {...gridProps}
     >
       <UI.Box
+        key="dot"
         gridArea="main"
         borderRadius="full"
         // bg={mode === 'tray' ? 'gray.400' : 'gray.100'}
         border="2px solid"
-        borderColor={mode === 'tray' ? 'gray.300' : 'blue.400'}
+        borderColor={mode === 'tray' ? 'gray.600' : 'blue.400'}
         w={6}
         h={6}
-        m={1}
+        // m={1}
         opacity={1}
         // boxShadow="inset 0 10px 20px rgba(0, 0, 0, 0.15)"
       />
       <UI.Box
+        key="drop-highlight-border"
         gridArea="main"
         borderRadius="lg"
         border="2px dashed"
         dash
         borderColor={dropZoneColor}
-        w="76px"
-        h="76px"
-        m={1}
-        transition="all 0.15s"
+        w={`${dropZoneBorderSize}px`}
+        h={`${dropZoneBorderSize}px`}
+        m={`${dropZoneBorderSize / -2}px`}
+        transition="all 0.3s"
+        position="relative"
+        zIndex={2}
+        pointerEvents="none"
       />
       <UI.Box
+        key="drop-highlight-bg"
         gridArea="main"
         borderRadius="md"
         bg={dropZoneColor}
-        w="68px"
-        h="68px"
-        m={1}
+        w={`${dropZoneSize}px`}
+        h={`${dropZoneSize}px`}
+        m={`${dropZoneSize / -2}px`}
         opacity={0.3}
-        transition="all 0.15s"
+        transition="all 0.1s"
+        position="relative"
+        zIndex={2}
+        pointerEvents="none"
       />
       {tile ? (
         <TileView mode={mode} gridArea="main" zIndex={1} tile={tile} />
@@ -276,9 +331,28 @@ const Score: React.FC = () => {
   );
 };
 
+const StrayDropsCatcher: React.FC<{ accept: string }> = ({ accept }) => {
+  const [, bodyDropRef] = useDrop(() => ({
+    accept,
+    drop: () => {
+      // do nothing
+    },
+  }));
+
+  React.useEffect(() => {
+    bodyDropRef(document.body);
+    return () => {
+      bodyDropRef(null);
+    };
+  }, [bodyDropRef]);
+
+  return null;
+};
+
 export const TileGamePage: React.FC = () => {
   return (
     <DndMultiProvider>
+      <StrayDropsCatcher accept={ItemTypes.tile} />
       <GameState.Provider value={null}>
         <NonScrollingFlex.Root>
           {/* <NonScrollingFlex.Child
@@ -296,19 +370,18 @@ export const TileGamePage: React.FC = () => {
             userSelect="none"
             bg="blue.600"
           >
-            <UI.Box w="360px" h="360px" p="20px">
-              <UI.SimpleGrid columns={4} spacing={0}>
-                {_.times(boardSize, (i) => (
-                  <TileSlot key={i} index={i} />
-                ))}
-              </UI.SimpleGrid>
-            </UI.Box>
+            <TileSlotGrid>
+              {_.times(boardSize, (i) => (
+                <TileSlot key={i} index={i} />
+              ))}
+            </TileSlotGrid>
           </NonScrollingFlex.Child>
           <NonScrollingFlex.Child
-            flex="1 1 auto"
+            flex="1 0 auto"
             alignItems="center"
             justifyContent="center"
             userSelect="none"
+            bg="gray.800"
           >
             {/* <UI.Flex
               direction="column"
@@ -328,13 +401,14 @@ export const TileGamePage: React.FC = () => {
                 boxShadow="inset 0 10px 20px rgba(0, 0, 0, 0.15)"
               > */}
             {/* <UI.SimpleGrid columns={4} spacing={0}> */}
-            <UI.HStack spacing={6}>
+            <UI.HStack spacing={0} pt="50px">
               {_.times(traySize, (i) => (
                 <TileSlot
                   key={i}
                   mode="tray"
                   index={i + boardSize}
                   // transform="scale(0.875)"
+                  w="104px"
                 />
               ))}
             </UI.HStack>
