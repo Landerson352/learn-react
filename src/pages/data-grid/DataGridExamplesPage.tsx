de3d41a3628adb830762beec4491f2e4259c337f
Lincoln Anderson <landerson@threefivetwo.com>
2022-12-20 09:15:07 -0500

    update datagrid example with working fetch

--- src/pages/data-grid/DataGridExamplesPage.tsx
+++ src/pages/data-grid/DataGridExamplesPage.tsx
@@ -2,18 +2,11 @@ import * as UI from '@chakra-ui/react';
 import React from 'react';
 import _ from 'lodash';
 import * as ReactTable from '@tanstack/react-table';
-import { DataTable } from './components/DataTable';
 
-const DataGridIntro: React.FC = () => {
-  return (
-    <UI.Box mb={8}>
-      <UI.Heading size="md" mb={4}>
-        Working with data grids
-      </UI.Heading>
-      <UI.Text mb={4}>...</UI.Text>
-    </UI.Box>
-  );
-};
+import { delay } from '../../helpers/delay';
+import { useTableWithFetch } from './useTableWithFetch';
+import { DataTable } from './components/DataTable';
+import { DataTablePagination } from './components/DataTablePagination';
 
 type UnitConversion = {
   fromUnit: string;
@@ -105,45 +98,35 @@ const columns = [
 ];
 
 const DataGridExamplesPage: React.FC = () => {
-  const exampleComponents = [DataGridIntro];
+  const [table, { fetching, loading }] = useTableWithFetch(
+    { columns }, // react-table options, must include column definitions
+    async (state) => {
+      // State contains updated sorting, filtering and pagination.
+      // This function is called whenever the state changes.
 
-  // TODO: replace this with fetch/axios hooks
-  const [data, setData] = React.useState(rawData);
-  const handleTableStateChange = (state: Partial<ReactTable.TableState>) => {
-    console.log(state);
-    setData((n) => _.shuffle(n));
-  };
+      // Mock fetching data based on state.
+      await delay(1000);
+      return _.sampleSize(rawData, state.pagination.pageSize);
+    }
+  );
+
+  // Fade the UI and disable clicks when fetching data.
+  const containerProps: UI.BoxProps = fetching
+    ? {
+        pointerEvents: 'none',
+        opacity: 0.5,
+      }
+    : {};
 
   return (
     <UI.Box p="4">
-      <UI.Heading size="3xl" mb={8}>
-        Data grid examples
-      </UI.Heading>
-
-      <UI.VStack alignItems="start" spacing={4}>
-        {/* <UI.SimpleGrid minChildWidth="400px" spacing={2} mb={8}> */}
-        {_.map(exampleComponents, (Component) => (
-          <UI.Box
-            key={Component.name}
-            bg="white"
-            borderRadius="8px"
-            p={6}
-            // minH="240px"
-            maxW="360px"
-            w="100%"
-          >
-            <Component />
-          </UI.Box>
-        ))}
-        {/* </UI.SimpleGrid> */}
-
+      <UI.VStack alignItems="stretch" spacing={4}>
         <UI.Box bg="white" borderRadius="8px" py={2}>
-          <DataTable
-            columns={columns}
-            data={data}
-            manual // aka. "I will update the data manually when onStateChange is called"
-            onStateChange={handleTableStateChange}
-          />
+          <UI.Box {...containerProps}>
+            {/* These display components are blind to the data fetching. */}
+            <DataTable skeleton={loading} table={table} />
+            <DataTablePagination table={table} />
+          </UI.Box>
         </UI.Box>
       </UI.VStack>
     </UI.Box>
--- src/pages/data-grid/components/DataTable.tsx
+++ src/pages/data-grid/components/DataTable.tsx
@@ -1,155 +1,14 @@
-import * as React from 'react';
 import * as UI from '@chakra-ui/react';
 import * as ReactTable from '@tanstack/react-table';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
-
-export type DataTableProps<Data extends object> = {
-  data: Data[];
-  columns: ReactTable.ColumnDef<Data, any>[];
-  tableOptions?: Partial<
-    Pick<ReactTable.TableOptions<Data>, 'onSortingChange'>
-  >;
-  onStateChange?: (state: Partial<ReactTable.TableState>) => void;
-  manual?: boolean;
-};
-
-// Notes:
-// When implementing table you must decide how to want to manage state.
-// Consider WHERE the sorting, filtering, and pagination occurs.
-// If it occurs in the component, you can leverage the automatic management of state.
-// If it occurs on the server, you will want to manage it manually.
+import _ from 'lodash';
 
 export function DataTable<Data extends object>({
-  data,
-  columns,
-  tableOptions,
-  onStateChange,
-  manual,
-}: DataTableProps<Data>): JSX.Element {
-  const [{ pageIndex, pageSize }, setPagination] =
-    React.useState<ReactTable.PaginationState>({
-      pageIndex: 0,
-      pageSize: 10,
-    });
-  const [sorting, setSorting] = React.useState<ReactTable.SortingState>([]);
-  const state = {
-    pagination: {
-      pageIndex,
-      pageSize,
-    },
-    sorting,
-  };
-
-  // callback when state object changes
-  // in "manual" implementations, the callback should update the data prop
-  React.useEffect(() => {
-    onStateChange?.(state);
-  }, [JSON.stringify(state)]);
-
-  const table = ReactTable.useReactTable({
-    columns,
-    data,
-    getCoreRowModel: ReactTable.getCoreRowModel(),
-
-    // These calls used by automatic management in local (component) state
-    getFilteredRowModel: ReactTable.getFilteredRowModel(),
-    getPaginationRowModel: ReactTable.getPaginationRowModel(),
-    getSortedRowModel: ReactTable.getSortedRowModel(),
-
-    // Local state is only used with automatic management, otherwise ignored
-    onPaginationChange: setPagination,
-    onSortingChange: setSorting,
-    state,
-
-    // When using manual management you must instead update the data
-    manualFiltering: manual,
-    manualPagination: manual,
-    manualSorting: manual,
-
-    ...tableOptions,
-  });
-
-  return (
-    <React.Fragment>
-      <DataTableView table={table} />
-      <DataTablePagination table={table} />
-    </React.Fragment>
-  );
-}
-
-export function DataTablePagination<Data extends object>({
+  skeleton,
   table,
 }: {
-  table: ReactTable.Table<Data>;
-}): JSX.Element {
-  return (
-    <div className="flex items-center gap-2">
-      <button
-        className="border rounded p-1"
-        onClick={() => table.setPageIndex(0)}
-        disabled={!table.getCanPreviousPage()}
-      >
-        {'<<'}
-      </button>
-      <button
-        className="border rounded p-1"
-        onClick={() => table.previousPage()}
-        disabled={!table.getCanPreviousPage()}
-      >
-        {'<'}
-      </button>
-      <button
-        className="border rounded p-1"
-        onClick={() => table.nextPage()}
-        disabled={!table.getCanNextPage()}
-      >
-        {'>'}
-      </button>
-      <button
-        className="border rounded p-1"
-        onClick={() => table.setPageIndex(table.getPageCount() - 1)}
-        disabled={!table.getCanNextPage()}
-      >
-        {'>>'}
-      </button>
-      <span className="flex items-center gap-1">
-        <div>Page</div>
-        <strong>
-          {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
-        </strong>
-      </span>
-      <span className="flex items-center gap-1">
-        | Go to page:
-        <input
-          type="number"
-          defaultValue={table.getState().pagination.pageIndex + 1}
-          onChange={(e) => {
-            const page = e.target.value ? Number(e.target.value) - 1 : 0;
-            table.setPageIndex(page);
-          }}
-          className="border p-1 rounded w-16"
-        />
-      </span>
-      <select
-        value={table.getState().pagination.pageSize}
-        onChange={(e) => {
-          table.setPageSize(Number(e.target.value));
-        }}
-      >
-        {[10, 20, 30, 40, 50].map((pageSize) => (
-          <option key={pageSize} value={pageSize}>
-            Show {pageSize}
-          </option>
-        ))}
-      </select>
-    </div>
-  );
-}
-
-export function DataTableView<Data extends object>({
-  table,
-}: {
+  skeleton?: boolean;
   table: ReactTable.Table<Data>;
 }): JSX.Element {
   return (
@@ -201,24 +60,48 @@ export function DataTableView<Data extends object>({
           </UI.Tr>
         ))}
       </UI.Thead>
-      <UI.Tbody>
-        {table.getRowModel().rows.map((row) => (
-          <UI.Tr key={row.id}>
-            {row.getVisibleCells().map((cell) => {
-              // see https://tanstack.com/table/v8/docs/api/core/column-def#meta to type this correctly
-              const meta: any = cell.column.columnDef.meta;
-              return (
-                <UI.Td key={cell.id} isNumeric={meta?.isNumeric}>
-                  {ReactTable.flexRender(
-                    cell.column.columnDef.cell,
-                    cell.getContext()
-                  )}
-                </UI.Td>
-              );
-            })}
-          </UI.Tr>
-        ))}
-      </UI.Tbody>
+      {skeleton ? (
+        <UI.Tbody>
+          {_.times(table.getState().pagination.pageSize, (i) => (
+            <UI.Tr key={i}>
+              {_.last(table.getHeaderGroups())?.headers.map((header) => {
+                return (
+                  <UI.Td key={header.id}>
+                    <UI.Box
+                      bg="gray.100"
+                      borderRadius="md"
+                      color="gray.100"
+                      _after={{
+                        content: '"—"',
+                        display: 'block',
+                      }}
+                    />
+                  </UI.Td>
+                );
+              })}
+            </UI.Tr>
+          ))}
+        </UI.Tbody>
+      ) : (
+        <UI.Tbody>
+          {table.getRowModel().rows.map((row) => (
+            <UI.Tr key={row.id}>
+              {row.getVisibleCells().map((cell) => {
+                // see https://tanstack.com/table/v8/docs/api/core/column-def#meta to type this correctly
+                const meta: any = cell.column.columnDef.meta;
+                return (
+                  <UI.Td key={cell.id} isNumeric={meta?.isNumeric}>
+                    {ReactTable.flexRender(
+                      cell.column.columnDef.cell,
+                      cell.getContext()
+                    )}
+                  </UI.Td>
+                );
+              })}
+            </UI.Tr>
+          ))}
+        </UI.Tbody>
+      )}
     </UI.Table>
   );
 }
